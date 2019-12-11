dd6ba1de09fe18f02b6cda4162242ab34ecf8ac6
Matheus <matheusouzatj@gmail.com>
2019-12-11 01:00:56 -0300

    WIP - fix the interval and add long break

--- src/Timer.js
+++ src/Timer.js
@@ -2,35 +2,50 @@ import React, { useState, useEffect } from 'react'
 import { formatTime } from './services/timerFormater'
 
 export const Timer = ({ pause, initialDate }) => {
-  const [time, setTime] = useState('25:00')
+  const [clock, setClock] = useState('25:00')
   const [working, setWorking] = useState(true)
-  const [workTime, setWorkTime] = useState(1)
+  const [minutesToRun, setMinutesToRun] = useState(0)
+  const [pomodoros, setPomodoros] = useState(0)
 
   useEffect(() => {
-    if (time === '0:0') {
-      setTime(working ? '5:00' : '25:00')
-      setWorkTime(working ? 5 : 25)
-      setWorking(!working)
+    if (pomodoros === 4) {
+      setPomodoros(0)
+      setMinutesToRun(minutesToRun + 15)
+      setClock('15:00')
       // eslint-disable-next-line
-      new Notification(working ? 'Take a break' : 'Focus on code!')
+      if (initialDate) new Notification('Take a long break')
+    } else {
+      if (working) setPomodoros(pomodoros + 1)
+      const minutesToAdd = working ? 25 : 5
+      setMinutesToRun(minutesToRun + minutesToAdd)
+      setClock(working ? '25:00' : '5:00')
+      // eslint-disable-next-line
+      if (initialDate) new Notification(working ?  'Focus on code!' : 'Take a break')
     }
+  }, [working])
 
-    if (!pause) {
-      setInterval(() => {
-        const teste = new Date().getTime()
-        const distance = (initialDate + workTime * 60000) - teste
-
+  useEffect(() => {
+    if (!pause && clock !== '0:0') {
+      const interval = setInterval(() => {
+        const now = new Date().getTime()
+        const distance = (initialDate + minutesToRun * 60000) - now
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
         const seconds = Math.floor((distance % (1000 * 60)) / 1000)
 
-        setTime(minutes + ':' + seconds)
-      }, 100)
+        if (minutes === 0 && seconds === 0) {
+          clearInterval(interval)
+          setWorking(!working)
+        } else {
+          setClock(minutes + ':' + seconds)
+        }
+      }, 900)
+      return () => clearInterval(interval)
     }
-  })
+  }, [clock, initialDate])
 
   return (
     <span>
-      {time}
+      {clock}
     </span>
   )
 }
